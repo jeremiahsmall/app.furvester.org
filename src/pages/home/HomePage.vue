43a9a7e9d93897e2cc24672f9d7549bc49df1dbd
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2018-05-08 02:26:23 +0200

    Add one-time prompt for iOS users to add app to home screen

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -4,10 +4,41 @@
 <script>
     import Navbar from '../../components/navbar/Navbar';
 
+    const promptRequired = () => {
+        if (! ['iPhone', 'iPad', 'iPod'].includes(navigator.platform)) {
+            return false;
+        }
+
+        if (false !== navigator.standalone) {
+            // If standalone is either true or undefined (no standalone support), we don't need to prompt.
+            return false;
+        }
+
+        if (localStorage.getItem('promptDismissed')) {
+            return false;
+        }
+
+        return true;
+    };
+
     export default {
         name: 'home-page',
         components: {
             Navbar,
+        },
+        data() {
+            return {
+                showPrompt: false,
+            };
+        },
+        mounted() {
+            this.showPrompt = promptRequired();
+        },
+        methods: {
+            dismissPrompt() {
+                localStorage.setItem('promptDismissed', true);
+                this.showPrompt = false;
+            },
         },
     };
 </script>
